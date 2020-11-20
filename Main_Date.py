6ebb673af9f303e7a8ce0a15873dc5e4b8d2f158
Julien <jm62140@gmail.com>
2020-11-20 10:47:05 +0100

    Vérification de date fini

--- Main_Date.py
+++ Main_Date.py
@@ -1,8 +1,18 @@
-import Bysextile as bysextile
-import mois as m
 
-année = 2300
-mois  = 1
-jour  = m.mois(mois,année)
-if jour != False:
-	print ("c'est un mois avec ",jour," jour")
+import Verification as v
+y = "y"
+while y =="y":
+	print ("Donner une date, Nous vous dirons si elle est correcte ou non")
+	print ("Donner l'année.")
+	année 	= int (input ())
+	print ("Donner le mois en nombre.")
+	mois  	= int (input ())
+	print ("Donner le jour en nombre.")
+	jour	= int (input())
+	valid 	= v.verification (jour,mois,année)
+	if valid :
+		print ("La Date est correcte")
+	else:
+		print ("La date est incorecte")
+	print ("pour continuer taper sur y.")
+	y = input()
